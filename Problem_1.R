c9e546d8952e7b41a0943bbea99277c14e1a273d
shdavitt <shdavitt@gmail.com>
2017-11-10 00:10:59 -0500

    Graphs made, but legend does not have the values directly on it(t                                                                                                                                  hey are in the order given in the exercise). I think putting the creation of mul                                                                                                                                  tiple dataframes within a for loop may be the answer.

--- Problem_1.R
+++ Problem_1.R
@@ -1,16 +1,15 @@
-#mypath=file.path("C:", "cygwin64", "home", "Shane", "IBC2017ND", "Tutorial","tut10","Intro_Biocomp_ND_317_Tutotial10", "constant_Kgraphs", paste("rVal", rList[i], ".jpg", sep="_"))
-#jpeg(file=mypath)
-#mytitle=paste("Constant K with r Value ")
-
 
 
 ### Problem 1 ###
 
+#Constant K, varying r
+
 #Load Packages
 library(deSolve)
 library(ggplot2)
-library(cowplot)
+library(reshape2)
 library(gridExtra)
+library(regexr)
 
 #Custom function
 popSim=function(t,y,p){
@@ -32,16 +31,60 @@ N0=10
 K=100
 times=1:100
 
-#Creating initial plot
-modelPopSim=ode(y=N0,times=times,func=popSim,parms=c(rList[1,1],K))
-modelPopSimOutput=data.frame(time=modelPopSim[,1], N=modelPopSim[,2])
+modelPopSimOutput=matrix(nrow = length(times), ncol=(1+length(rList)))
+modelPopSimOutput[,1]=c(1:100)
 
-a=ggplot(modelPopSimOutput,aes(x=time,y=N))+geom_line()+theme_classic()
-
-for (i in rList[1,2:5]){
-  modelPopSim=ode(y=N0,times=times,func=popSim,parms=c(i,K))
-  modelPopSimOutput=data.frame(time=modelPopSim[,1], N=modelPopSim[,2])
-  
-  b=ggplot(modelPopSimOutput,aes(x=time,y=N))+geom_line()+theme_classic()+labs(title=paste("Constant K with r Value "), rList[i], sep="_")
-  a=grid.arrange(a,b,ncol=5)
+for (i in 1:length(rList[1,])){
+  modelPopSim=ode(y=N0,times=times,func=popSim,parms=c(rList[i],K))
+  modelPopSimOutput[,(1+i)]=modelPopSim[,2]
 }
+
+modelPopSimOutput_m=melt(modelPopSimOutput)
+
+ggplot()+
+  geom_line(data = modelPopSimOutput_m[modelPopSimOutput_m$Var2 != 1,], aes(x = Var1, y = value, group=Var2, color=factor(Var2)))+
+  labs(title="Constant K with Varying r Values", x="time", y="N")
+
+#Constant r, varying K
+
+KVals=c(10,50,100)
+KList=array(data=KVals, dim=c(1,3))
+
+N0=1
+r=0.2
+
+modelPopSimOutput=matrix(nrow = length(times), ncol=(1+length(KList)))
+modelPopSimOutput[,1]=c(1:100)
+
+for (i in 1:length(KList[1,])){
+  modelPopSim=ode(y=N0,times=times,func=popSim,parms=c(r,KList[i]))
+  modelPopSimOutput[,(1+i)]=modelPopSim[,2]
+}
+
+modelPopSimOutput_m=melt(modelPopSimOutput)
+
+ggplot()+
+  geom_line(data = modelPopSimOutput_m[modelPopSimOutput_m$Var2 != 1,], aes(x = Var1, y = value, group=Var2, color=factor(Var2)))+
+  labs(title="Constant r with Varying K Values", x="time", y="N")
+
+#Varying N0
+
+NVals=c(1,50,100)
+NList=array(data=NVals, dim=c(1,3))
+
+r=0.1
+K=50
+
+modelPopSimOutput=matrix(nrow = length(times), ncol=(1+length(NList)))
+modelPopSimOutput[,1]=c(1:100)
+
+for (i in 1:length(NList[1,])){
+  modelPopSim=ode(y=NList[i],times=times,func=popSim,parms=c(r,K))
+  modelPopSimOutput[,(1+i)]=modelPopSim[,2]
+}
+
+modelPopSimOutput_m=melt(modelPopSimOutput)
+
+ggplot()+
+  geom_line(data = modelPopSimOutput_m[modelPopSimOutput_m$Var2 != 1,], aes(x = Var1, y = value, group=Var2, color=factor(Var2)))+
+  labs(title="Constant K and r with Varying N0 Values", x="time", y="N")
